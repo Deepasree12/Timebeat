b0689e74de693d63ddd87d9f403504dd11a4340e
Deepasree A <deepasree.a@gmail.com>
2023-11-28 08:48:29 +0530

    cart changed

--- timebeat/user/context_processors.py
+++ timebeat/user/context_processors.py
@@ -2,6 +2,7 @@ from cart.models import *
 from wishlist.models import *
 
 def calculate_count(request):
-  cartitems=CartItem.objects.filter(cart=request.user.cart)
-  wishlist=WishlistItem.objects.filter(wished_item=request.user.wishlist)
-  return {"cartitems":cartitems,"wishlist":wishlist}
+  if request.user.is_authenticated:
+    cartitems=CartItem.objects.filter(cart=request.user.cart)
+    wishlist=WishlistItem.objects.filter(wished_item=request.user.wishlist)
+    return {"cartitems":cartitems,"wishlist":wishlist}
